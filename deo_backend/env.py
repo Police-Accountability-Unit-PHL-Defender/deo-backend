28dc65181bcd94e5a54a1b4a89c59c11e2038111
Stephen Suffian <stephen.suffian@pm.me>
2024-07-12 20:29:57 -0400

    Update to July 1 data

--- deo_backend/env.py
+++ deo_backend/env.py
@@ -1,5 +1,5 @@
 import os
 
-MOST_RECENT_YEAR = os.environ.get("MOST_RECENT_YEAR", "2023")
-MOST_RECENT_QUARTER_START = os.environ.get("MOST_RECENT_QUARTER_START", "2023-10-01")
-DB_FILENAME = os.environ.get("DB_FILENAME", "open_data_philly_2024_03_16.db")
+MOST_RECENT_YEAR = os.environ.get("MOST_RECENT_YEAR", "2024")
+MOST_RECENT_QUARTER_START = os.environ.get("MOST_RECENT_QUARTER_START", "2024-04-01")
+DB_FILENAME = os.environ.get("DB_FILENAME", "open_data_philly_2024_07_01.db")
